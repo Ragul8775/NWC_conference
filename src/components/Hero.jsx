dac283d9311a50fea620771b00f6e18ebf9f4786
Ragul8775 <rb7439@srmist.edu.in>
2024-03-17 01:36:05 +0530

    Navbar,hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className="relative bg-gradient-to-r py-24 md:py-36 from-purple-600 to-blue-600 text-white overflow-hidden">
+        <div className="absolute inset-0">
+          <img
+            src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw2fHxjb2RlfGVufDB8MHx8fDE2OTQwOTg0MTZ8MA&ixlib=rb-4.0.3&q=80&w=1080"
+            alt="Background Image"
+            className="object-cover object-center w-full h-full"
+          />
+          <div className="absolute inset-0 bg-black opacity-50"></div>
+        </div>
+
+        <div className="relative z-10 flex flex-col justify-center items-center h-full text-center">
+          <h1 className=" text-3xl sm:text-4xl font-bold leading-tight mb-4 text-balance px-4">
+            IEEE International Conference on Networking and Communications 2023
+            (ICNWC 2023) – Hybrid Modee
+          </h1>
+
+          <a
+            href="#"
+            className="bg-yellow-400 text-gray-900 hover:bg-yellow-300 py-2 px-6 rounded-full text-lg font-semibold transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg"
+          >
+            Get Started
+          </a>
+        </div>
+      </div>
+      <div className="flex flex-col sm:flex-row justify-center items-center">
+        <div className="flex gap-2 px-4 py-4 text-lg font-semibold">
+          <span>
+            <svg
+              class="w-6 h-6 text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M11.906 1.994a8.002 8.002 0 0 1 8.09 8.421 7.996 7.996 0 0 1-1.297 3.957.996.996 0 0 1-.133.204l-.108.129c-.178.243-.37.477-.573.699l-5.112 6.224a1 1 0 0 1-1.545 0L5.982 15.26l-.002-.002a18.146 18.146 0 0 1-.309-.38l-.133-.163a.999.999 0 0 1-.13-.202 7.995 7.995 0 0 1 6.498-12.518ZM15 9.997a3 3 0 1 1-5.999 0 3 3 0 0 1 5.999 0Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </span>
+          <h2>SRMIST</h2>
+        </div>
+        <div className="flex gap-2 px-4 py-4 text-lg font-semibold">
+          <span>
+            <svg
+              class="w-6 h-6 text-gray-800 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M7.978 4a2.553 2.553 0 0 0-1.926.877C4.233 6.7 3.699 8.751 4.153 10.814c.44 1.995 1.778 3.893 3.456 5.572 1.68 1.679 3.577 3.018 5.57 3.459 2.062.456 4.115-.073 5.94-1.885a2.556 2.556 0 0 0 .001-3.861l-1.21-1.21a2.689 2.689 0 0 0-3.802 0l-.617.618a.806.806 0 0 1-1.14 0l-1.854-1.855a.807.807 0 0 1 0-1.14l.618-.62a2.692 2.692 0 0 0 0-3.803l-1.21-1.211A2.555 2.555 0 0 0 7.978 4Z" />
+            </svg>
+          </span>
+          <h2>+91-9626611060 | +91-8838222378</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
