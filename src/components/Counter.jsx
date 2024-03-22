fa77f0be619066b1e9b0f763c498eee95731d52e
Ragul_8775 <101054801+Ragul8775@users.noreply.github.com>
2024-03-22 10:54:11 +0530

    Update Counter.jsx

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,19 +6,19 @@ const Counter = () => {
     <div className="bg-gradient-to-r from-primary to-primaryLight py-14   flex justify-around shadow-inner">
       <div className="flex flex-col sm:flex-row gap-6 md:gap-12 lg:gap-20 xl:gap-32">
         <div className=" text-center font-bold text-white">
-          <RollNo number={705} />
+          <RollNo number={0} />
           <h2 className="text-sm font-bold">Total Entries</h2>
         </div>
         <div className="text-6xl text-center font-bold text-white">
-          <RollNo number={652} />
+          <RollNo number={0} />
           <h2 className="text-sm font-bold">Total No.of full paper</h2>
         </div>
         <div className="text-6xl text-center font-bold text-white">
-          <RollNo number={26} />
+          <RollNo number={0} />
           <h2 className="text-sm font-bold">Foreign Author Paper</h2>
         </div>
         <div className="text-6xl text-center font-bold text-white">
-          <RollNo number={316} />
+          <RollNo number={0} />
           <h2 className="text-sm font-bold">Total No of accepted papers</h2>
         </div>
       </div>
